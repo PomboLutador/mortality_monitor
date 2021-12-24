e92cd476d09e579e2d9222c3bfb4e0543630c565
Pombo Lutador <tizian.bluntschli@gmail.com>
2021-12-24 16:22:37 +0000

    Use methods param & more named constants
    and unpack function to get mortality data

--- mortality_monitor/server.py
+++ mortality_monitor/server.py
@@ -22,51 +22,47 @@ from mortality_monitor.util import (
 app = Flask(__name__)
 CORS(app)
 
+YEAR = "year"
 DATA_FOLDER = "data"
 ARCHIVE_FOLDER = "archive"
 MORTALITY_DATA_FILENAME = "mortality_data"
+CACHE = DataFrameFileCache(data_folder=DATA_FOLDER, archive_folder=ARCHIVE_FOLDER)
 
 
-def _get_mortality_data(
-    cache: DataFrameFileCache, filename: str = MORTALITY_DATA_FILENAME
-) -> pd.DataFrame:
-    try:
-        mortality_data = cache.get_data(
-            filename, read_function=read_csv_with_weekly_period
-        )
-    except OSError:
-        mortality_data = get_mortality_data(
-            geo="country", ages=get_all_age_groups_for_query()
-        )
-        cache.put_data(data=mortality_data, filename=filename)
-    finally:
-        return mortality_data
-
-
-cache = DataFrameFileCache(data_folder=DATA_FOLDER, archive_folder=ARCHIVE_FOLDER)
-mortality_data = _get_mortality_data(cache=cache)
-
-
-@app.route("/available_geos")
-def available_geos():
-    return jsonify(list(mortality_data.reset_index()[GEO_COLUMN].unique()))
-
-
-@app.route("/available_ages")
-def available_ages():
-    return jsonify(QUERY_AGE_TO_DATA_AGE)
-
-
-@app.route("/available_years")
-def available_years():
-    available_years = (
-        mortality_data.reset_index()[PERIOD_COLUMN]
-        .map(lambda period: int(period.year))
-        .drop_duplicates()
-        .values.tolist()
+try:
+    mortality_data = CACHE.get_data(
+        MORTALITY_DATA_FILENAME, read_function=read_csv_with_weekly_period
     )
-    available_years.sort(reverse=True)
-    return jsonify(available_years)
+except OSError:
+    mortality_data = get_mortality_data(
+        geo="country", ages=get_all_age_groups_for_query()
+    )
+    CACHE.put_data(data=mortality_data, filename=MORTALITY_DATA_FILENAME)
+
+
+@app.route("/available_geos", methods=["GET"])
+def available_geos():
+    if request.method == "GET":
+        return jsonify(list(mortality_data.reset_index()[GEO_COLUMN].unique()))
+
+
+@app.route("/available_ages", methods=["GET"])
+def available_ages():
+    if request.method == "GET":
+        return jsonify(QUERY_AGE_TO_DATA_AGE)
+
+
+@app.route("/available_years", methods=["GET"])
+def available_years():
+    if request.method == "GET":
+        available_years = (
+            mortality_data.reset_index()[PERIOD_COLUMN]
+            .map(lambda period: int(period.year))
+            .drop_duplicates()
+            .values.tolist()
+        )
+        available_years.sort(reverse=True)
+        return jsonify(available_years)
 
 
 @app.route("/excess_deaths", methods=["POST"])
@@ -79,9 +75,9 @@ def excess_deaths():
             ages=user_input[AGE_COLUMN],
         )
         expected_deaths = get_expected_deaths(deaths=deaths).pipe(
-            _filter_on_year, year=user_input["year"]
+            _filter_on_year, year=user_input[YEAR]
         )
-        deaths = deaths.pipe(_filter_on_year, year=user_input["year"])
+        deaths = deaths.pipe(_filter_on_year, year=user_input[YEAR])
 
         excess_deaths = deaths - expected_deaths
         periods = deaths.index
@@ -97,14 +93,12 @@ def excess_deaths():
 
         return jsonify(
             {
-                "weekly_data": {
-                    "deaths": deaths.values.round().tolist(),
-                    "label": [f"{period.year}/{period.week}" for period in periods],
-                    "excess_deaths": excess_deaths.values.round().tolist(),
-                    "expected_deaths": expected_deaths.values.round().tolist(),
-                    "above_expectation_deaths": above_expectation_deaths,
-                    "below_expectation_deaths": below_expectation_deaths,
-                }
+                "deaths": deaths.values.round().tolist(),
+                "label": [f"{period.year}/{period.week}" for period in periods],
+                "excess_deaths": excess_deaths.values.round().tolist(),
+                "expected_deaths": expected_deaths.values.round().tolist(),
+                "above_expectation_deaths": above_expectation_deaths,
+                "below_expectation_deaths": below_expectation_deaths,
             }
         )
 
@@ -122,17 +116,17 @@ def yearly_deaths():
             )
             .reset_index()
             .assign(
-                year=lambda df: df["period"].map(lambda period: period.year),
-                week=lambda df: df["period"].map(lambda period: period.week),
+                year=lambda df: df[PERIOD_COLUMN].map(lambda period: period.year),
+                week=lambda df: df[PERIOD_COLUMN].map(lambda period: period.week),
             )
             .query("week <= @max_week")
-            .groupby(by=["year"], as_index=False)[DEATHS_COLUMN]
+            .groupby(by=[YEAR], as_index=False)[DEATHS_COLUMN]
             .sum()
         )
         return jsonify(
             {
                 "yearly_deaths": {
-                    "years": deaths_per_year["year"].values.tolist(),
+                    "years": deaths_per_year[YEAR].values.tolist(),
                     "actual_deaths": deaths_per_year[DEATHS_COLUMN].values.tolist(),
                     "max_week": max_week,
                 }
@@ -146,7 +140,7 @@ def _filter_on_year(data: pd.Series, year: int) -> pd.Series:
         data.reset_index()  # type: ignore
         .assign(year=lambda df: df[PERIOD_COLUMN].map(lambda period: period.year))
         .query("year >= @year")
-        .drop(columns=["year"])
+        .drop(columns=[YEAR])
         .set_index(PERIOD_COLUMN)[column_name]
     )
 

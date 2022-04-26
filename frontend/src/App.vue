c8c9c5e1782e7f8acfa8a68ce2ff93c5867071f4
Pombo Lutador <tizian.bluntschli@gmail.com>
2022-04-26 22:30:16 +0100

    Update main files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,115 @@
 <template>
   <v-app>
     <v-app-bar app color="indigo darken-1" dark>
-      <h2>Mortality Monitor</h2>
-      <v-spacer></v-spacer>
-      <v-btn
-        href="https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=demo_r_mweek3&lang=en"
-        target="_blank"
-        text
-      >
-        <span>Data source</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <v-btn
-        href="https://github.com/PomboLutador/mortality_monitor"
-        target="_blank"
-        text
-      >
-        <span>Source code (GitHub)</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-col cols="3">
+        <v-row class="text-left">
+          <v-col cols="3">
+            <router-link
+              style="text-decoration: none; font-size: x-large"
+              to="/"
+            >
+              <span class="mb-4 white--text"> Home </span>
+            </router-link>
+          </v-col>
+          <v-col cols="7">
+            <router-link
+              style="text-decoration: none; font-size: x-large"
+              to="/mortality_monitor"
+            >
+              <span class="mb-4 white--text"> Mortality Monitor </span>
+            </router-link>
+          </v-col>
+          <v-col cols="2"> </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="5"></v-col>
+      <v-col cols="4">
+        <v-row class="text-right">
+          <v-col cols="3"> </v-col>
+          <v-col cols="3"></v-col>
+          <v-col cols="5">
+            <v-btn
+              href="https://github.com/PomboLutador/mortality_monitor"
+              target="_blank"
+              text
+            >
+              GitHub (PomboLutador)
+              <v-icon>mdi-open-in-new</v-icon>
+            </v-btn></v-col
+          >
+          <v-col cols="1"> </v-col>
+        </v-row>
+      </v-col>
     </v-app-bar>
+    <v-navigation-drawer
+      color="indigo darken-1 bx-5"
+      permanent
+      floating
+      app
+      dark
+    >
+      <div v-if="currentRoute == '/mortality_monitor'">
+        <h2 class="text-left px-5 mt-4 white--text">Introduction</h2>
+        <p class="text-left px-5 white--text">
+          This website allows tracking of all-cause mortality in various age
+          groups on a weekly basis for a variety of European countries.
+        </p>
+        <h2 class="px-5 white--text">Methodology</h2>
+        <p class="text-left px-5 white--text">
+          Weekly mortality data is pulled from EUROSTAT's
+          <i>demo_r_mweek3</i> table. To compute expected deaths for a given
+          weekly period <b>p</b>, the average of the 3 previous datapoints with
+          the same calendar week is computed. Datapoints not within mean +-
+          standard deviation are disregarded.
+          <br />
+          <br />
+          A linear regression is fit through the prior 3 years of yearly deaths,
+          starting from <b>p - 104 weeks</b> to <b>p - 52 weeks</b>. The growth
+          of that linear regression is applied to the above calculated mean of
+          previous observations to arrive at the expectation value for <b>p</b>.
+          The moving average over 4 weeks is computed to smooth out the
+          resulting expected deaths time series.
+        </p>
+
+        <v-btn
+          href="https://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=demo_r_mweek3&lang=en"
+          target="_blank"
+          text
+        >
+          Eurostat
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+        <v-btn
+          href="https://github.com/PomboLutador/mortality_monitor"
+          target="_blank"
+          text
+        >
+          Source code
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </div>
+      <div v-if="currentRoute == '/'">
+        <h2 class="text-left px-5 mt-4 white--text">Welcome!</h2>
+        <p class="text-left px-5 white--text">
+          Some text and general introduction.
+        </p>
+      </div>
+    </v-navigation-drawer>
     <v-main>
-      <home_page />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import home_page from './components/home_page.vue';
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    home_page,
+  name: "App",
+  components: {},
+  computed: {
+    currentRoute() {
+      return this.$route.path;
+    },
   },
 };
 </script>
---
+++ frontend/src/components/mortality_monitor.vue
@@ -0,0 +1,299 @@
+<template>
+  <v-container>
+    <v-row class="text-center mt-2">
+      <v-col cols="1"></v-col>
+      <v-col cols="3">
+        <v-select
+          v-model="country_selection"
+          :items="geo_options"
+          item-value="name"
+          item-text="name"
+          label="Region"
+          outlined
+        ></v-select>
+      </v-col>
+      <v-col cols="4">
+        <v-container fluid>
+          <v-select
+            v-model="age_groups_selection"
+            :items="available_age_groups"
+            label="Age groups"
+            item-text="name"
+            item-value="value"
+            multiple
+          ></v-select>
+        </v-container>
+      </v-col>
+      <v-col cols="2">
+        <v-select
+          v-model="year_selection"
+          :items="available_years"
+          label="Starting year"
+          outlined
+        ></v-select>
+      </v-col>
+      <v-col cols="1">
+        <v-select
+          v-model="calendar_week_selection"
+          :items="calendar_week_options"
+          label="Calendar week"
+          outlined
+        ></v-select>
+      </v-col>
+      <v-col cols="1"> </v-col>
+    </v-row>
+    <v-row
+      ><v-col cols="5"></v-col>
+      <v-col cols="2">
+        <center>
+          <v-btn
+            @click="
+              get_excess_deaths_data(
+                country_selection,
+                age_groups_selection,
+                year_selection
+              );
+              get_yearly_deaths_data(
+                country_selection,
+                age_groups_selection,
+                year_selection,
+                calendar_week_selection
+              );
+            "
+            depressed
+            elevation="2"
+            class="font-weight-bold"
+            >Draw!
+          </v-btn>
+        </center>
+      </v-col>
+      <v-col cols="5"></v-col>
+    </v-row>
+    <v-row class="text-left">
+      <v-col class="mb-4" cols="12">
+        <h1
+          v-if="
+            available_geos_query_is_successful == null &&
+            available_ages_query_is_successful == null &&
+            available_years_query_is_successful == null
+          "
+        >
+          Please wait while the data is being fetched
+        </h1>
+        <v-overlay
+          v-if="
+            available_geos_query_is_successful == null &&
+            available_ages_query_is_successful == null &&
+            available_years_query_is_successful == null
+          "
+        ></v-overlay>
+        <v-progress-linear
+          indeterminate
+          color="primary"
+          v-if="
+            available_geos_query_is_successful == null &&
+            available_ages_query_is_successful == null &&
+            available_years_query_is_successful == null
+          "
+        ></v-progress-linear>
+        <h2
+          v-if="excess_deaths_request_successful"
+          class="font-weight-bold mb-3 text-center"
+        >
+          Weekly all-cause mortality for country {{ country_selection }}
+        </h2>
+        <excess_deaths_chart
+          v-if="excess_deaths_request_successful"
+          :chartData="datacollection_excess_deaths_chart"
+          class="mt-3"
+        ></excess_deaths_chart>
+      </v-col>
+    </v-row>
+    <v-row class="text-left">
+      <v-col cols="12">
+        <h2
+          v-if="yearly_deaths_request_successful"
+          class="text-center font-weight-bold mb-3"
+        >
+          Aggregated yearly deaths for country
+          {{ country_selection }} until calendar week
+          {{ calendar_week_selection }}
+        </h2>
+        <h6
+          v-if="yearly_deaths_request_successful"
+          class="text-center font-weight-bold mb-3"
+        >
+          <i
+            >(If current year hasn't passed calendar week
+            {{ calendar_week_selection }} yet, all available data for that year
+            is considered.)</i
+          >
+        </h6>
+        <yearly_deaths_chart
+          v-if="yearly_deaths_request_successful"
+          :chartData="datacollection_yearly_deaths_chart"
+        ></yearly_deaths_chart>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import excess_deaths_chart from "./excess_deaths_chart.vue";
+import yearly_deaths_chart from "./yearly_deaths_chart.vue";
+
+export default {
+  name: "mortality_monitor",
+
+  data: () => ({
+    // Query flags
+    excess_deaths_request_successful: null,
+    yearly_deaths_request_successful: null,
+    available_geos_query_is_successful: null,
+    available_ages_query_is_successful: null,
+    available_years_query_is_successful: null,
+
+    // UI selections
+    calendar_week_selection: 53,
+    year_selection: null,
+    country_selection: "",
+    age_groups_selection: "",
+
+    // UI options
+    calendar_week_options: Array.from({ length: 53 }, (x, i) => i + 1),
+    geo_options: [],
+    available_age_groups: [],
+    available_years: [],
+
+    // Chart data
+    labels: [],
+    datacollection_excess_deaths_chart: {},
+    datacollection_yearly_deaths_chart: {},
+  }),
+
+  components: {
+    excess_deaths_chart,
+    yearly_deaths_chart,
+  },
+
+  mounted() {
+    // Get available geos
+    const available_geos_url = "<placeholder>/available_geos";
+    axios
+      .get(available_geos_url, {})
+      .then((res) => {
+        this.geo_options = res.data.sort();
+        this.available_geos_query_is_successful = true;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Get available age groups
+    const available_ages_url = "<placeholder>/available_ages";
+    axios
+      .get(available_ages_url, {})
+      .then((res) => {
+        this.available_age_groups = Object.entries(res.data).map((arr) => ({
+          name: arr[1],
+          value: arr[0],
+        }));
+        this.available_ages_query_is_successful = true;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Get available years
+    const available_years_url = "<placeholder>/available_years";
+    axios
+      .get(available_years_url, {})
+      .then((res) => {
+        this.available_years = res.data;
+        this.available_years_query_is_successful = true;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  methods: {
+    get_excess_deaths_data(geo_choice, age_choices, year_choice) {
+      const backend_url = "<placeholder>/excess_deaths";
+      axios
+        .post(backend_url, {
+          geo: geo_choice,
+          age: age_choices,
+          year: year_choice,
+        })
+        .then((res) => {
+          this.labels = res.data.label;
+          this.deaths = res.data.deaths;
+          this.expected_deaths = res.data.expected_deaths;
+          this.above_expectation_deaths = res.data.above_expectation_deaths;
+          this.below_expectation_deaths = res.data.below_expectation_deaths;
+          this.datacollection_excess_deaths_chart = {
+            labels: this.labels,
+            datasets: [
+              {
+                pointBackgroundColor: "#000000",
+                pointBorderColor: "#000000",
+                borderWidth: 2,
+                pointRadius: 1,
+                type: "line",
+                backgroundColor: "#000000",
+                borderColor: "#000000",
+                label: "Expected deaths",
+                data: this.expected_deaths,
+                fill: false,
+                lineTension: 0,
+              },
+              {
+                backgroundColor: "#1A237E",
+                label: "Actual deaths up to expected",
+                data: this.deaths,
+              },
+              {
+                backgroundColor: "#4CAF50",
+                label: "Below expectation deaths",
+                data: this.below_expectation_deaths,
+              },
+              {
+                backgroundColor: "#F44336",
+                label: "Excess deaths",
+                data: this.above_expectation_deaths,
+              },
+            ],
+          };
+        });
+      this.excess_deaths_request_successful = true;
+    },
+    get_yearly_deaths_data(geo_choice, age_choices, year_choice, max_week) {
+      const backend_url = "<placeholder>/yearly_deaths";
+      axios
+        .post(backend_url, {
+          geo: geo_choice,
+          age: age_choices,
+          year: year_choice,
+          max_week,
+        })
+        .then((res) => {
+          this.labels = res.data.yearly_deaths.years;
+          this.actual_deaths = res.data.yearly_deaths.actual_deaths;
+          this.max_week = res.data.yearly_deaths.max_week;
+          this.datacollection_yearly_deaths_chart = {
+            labels: this.labels,
+            datasets: [
+              {
+                backgroundColor: "#1A237E",
+                label: "Yearly deaths",
+                data: this.actual_deaths,
+              },
+            ],
+          };
+        });
+      this.yearly_deaths_request_successful = true;
+    },
+  },
+};
+</script>

504429ebbf912bb053c91d9473f6404bc26d5ab5
Pombo Lutador <tizian.bluntschli@gmail.com>
2022-04-26 22:22:53 +0100

    Add router to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
-
+import mortality_monitor from "./components/mortality_monitor.vue";
+import home from "./components/home_page.vue";
 Vue.config.productionTip = false;
 
+const routes = [
+  { path: '/', component: home },
+  { path: '/mortality_monitor', component: mortality_monitor },
+
+]
+
+const router = new VueRouter({
+
+  mode: 'history',
+  routes // short for `routes: routes`
+})
+
+Vue.use(VueRouter);
 new Vue({
+  router,
   vuetify,
-  render: (h) => h(App),
+  render: (h) => {
+    return h(App);
+  },
 }).$mount('#app');
